d0716d35a05f4ff8e5820124d80c330681644ebe
Asma <asmaoualmakran@gmail.com>
2018-11-07 14:40:09 +0100

    function couple! added to the class DrivingObjet%

--- NMBS/drivingObject.rkt
+++ NMBS/drivingObject.rkt
@@ -22,13 +22,14 @@
            [successorID 'uninitialised]
            [trainID 'uninitialised])
 
-;--------------------------------------------------------------
+;------------------------------------------------------------------
 ; Function: initialised?
 ; Parameters: n/a
-; Ouput: boolean
-;  use: Determine wheter the object is completely instatiated.
+; Ouput:
+;    boolean
+;      Use: Determine wheter the object is completely instatiated.
 ; Use: Determine wheter the object is completely instatiated.
-;--------------------------------------------------------------
+;------------------------------------------------------------------
 
     (define/public (initialised?)
       (and (not (eq? ID 'uninitialised))
@@ -38,8 +39,9 @@
 
 ;-------------------------------------------------------
 ; Function: setID!
-; Parameters: symbol: symbol
-;     Use: The identification of the driving object.
+; Parameters:
+;     symbol: symbol
+;       Use: The identification of the driving object.
 ; Output: n/a
 ; Use: Setting the identification of a driving object.
 ;-------------------------------------------------------
@@ -53,8 +55,9 @@
 ;------------------------------------------------
 ; Funcion: getID
 ; Parameters: n/a
-; Output: ID: symbol
-;  Use: The ID of the driving object.
+; Output:
+;    ID: symbol
+;     Use: The ID of the driving object.
 ; Use: Retreiving the ID of the driving object.
 ;------------------------------------------------
 
@@ -63,13 +66,14 @@
           ID
           (error "DrivingObject% getID: object not initialised, please initialise before use" ID)))
 
-;------------------------------------------------------------------------------
+;----------------------------------------------------------------------------------
 ; Function: setPredecessorID!
-; Parameters: symbol: symbol
-;   Use: The identification of the predecessor of the current driving object.
+; Parameters:
+;      symbol: symbol
+;        Use: The identification of the predecessor of the current driving object.
 ; Output: n/a
 ; Use: Setting the identification of the predecessing driving object.
-;------------------------------------------------------------------------------
+;----------------------------------------------------------------------------------
 
     (define/public (setPredecessorID! symbol)
       (if (symbol? symbol)
@@ -79,8 +83,9 @@
 ;---------------------------------------------------------------------------
 ; Function: getPredecessorID
 ; Parameters: n/a
-; Output: predecessorID: symbol
-;    Use: The identification of the predecessing driving object.
+; Output:
+;    predecessorID: symbol
+;        Use: The identification of the predecessing driving object.
 ; Use: Retrieving the identification of the predecessing driving object.
 ;---------------------------------------------------------------------------
 
@@ -89,13 +94,14 @@
           predecessorID
           (error "DrivingObject% getPredecessorID: object not initialised, please initialise before use" ID)))
 
-;--------------------------------------------------------------
+;------------------------------------------------------------------
 ; Function: setSuccessorID!
-; Parameters: symbol: symbol
-;    Use: The identification of the successing driving object.
+; Parameters:
+;     symbol: symbol
+;       Use: The identification of the successing driving object.
 ; Output: n/a
 ; Use: Setting the successing driving object's identification.
-;--------------------------------------------------------------
+;------------------------------------------------------------------
 
     (define/public (setSuccessorID! symbol)
       (if (symbol? symbol)
@@ -105,8 +111,9 @@
 ;---------------------------------------------------------------------
 ; Function: getSuccessorID
 ; Parameters: n/a
-; Output: successorID: symbol
-;   Use: The identification of the successing driving object.
+; Output:
+;    successorID: symbol
+;      Use: The identification of the successing driving object.
 ; Use: Retrieve the identification of the successing driving object.
 ;---------------------------------------------------------------------
 
@@ -117,8 +124,9 @@
 
 ;-----------------------------------------------------------------------------------------
 ; Function: setTrainID!
-; Parameters: symbol: symbol
-;     Use: The identification of the train where the driving object is associated with.
+; Parameters:
+;    symbol: symbol
+;      Use: The identification of the train where the driving object is associated with.
 ; Output: n/a
 ; Use: Setting the identification associating train of the driving object.
 ;-----------------------------------------------------------------------------------------
@@ -131,8 +139,9 @@
 ;-------------------------------------------------------------
 ; Function: getTrainID
 ; Parameters: n/a
-; Output: trainID
-;   Use: Identifiaction of the associating train.
+; Output:
+;    trainID: symbol
+;      Use: Identifiaction of the associating train.
 ; Use: Retrieve the identification of the assocciating train.
 ;-------------------------------------------------------------
 
@@ -140,6 +149,29 @@
       (if (initialised?)
           trainID
           (error "DrivingObject% getTrainID: object is not initialised, please initialise before use" ID)))
+
+;------------------------------------------------------------------------------------------------
+; Function: couple!
+; Parameters:
+;    train: symbol
+;      Use: The identification of the associating train. This can not be 'none
+;    pre: symbol
+;     Use: The identification of the predecessing driving object.
+;    succ: symbol
+;     Use: The identificationf of the successing driving object.
+; Use: Connecting a driving object to a train, and potentionally a successor and predecessor.
+;      if there isn't a successor and predecessor this can be set to the symbol 'none. 
+;------------------------------------------------------------------------------------------------
+
+    (define/public (couple! train pre succ)
+      (if (and (and (symbol? train)(not (eq? train 'none)))   ; A driving object must be connected to a train, this can't be none
+               (symbol? pre)
+               (symbol? succ))
+          (begin
+            (set! trainID train)
+            (set! predecessorID pre)
+            (set! successorID succ))
+          (error "DrivingObject% couple!: contract violation symbol expected received" train pre succ)))
 
    
     ))
