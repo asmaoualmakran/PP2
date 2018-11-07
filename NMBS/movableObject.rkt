e4ebc6a26bfca740661db395bb86a59158f87608
Asma <asmaoualmakran@gmail.com>
2018-11-07 19:48:41 +0100

    Renameing drivingObject to movableObject

--- NMBS/movableObject.rkt
+++ NMBS/movableObject.rkt
@@ -2,16 +2,16 @@
 
 (require racket/class)
 
-(provide DrivingObject%)
+(provide MoveableObject%)
 
 ;-------------------------------------------------------
-; Class: DrivingObject%
+; Class: MoveableObject%
 ; Parameters: n/a
-; Output: DrivingObject% : object
+; Output: MoveableObject% : object
 ; Use: Super class for creating Locomotive and Railcar.
 ;-------------------------------------------------------
 
-(define DrivingObject%
+(define MoveableObject%
   (class object%
     (super-new)
 
@@ -41,100 +41,100 @@
 ; Function: setID!
 ; Parameters:
 ;     symbol: symbol
-;       Use: The identification of the driving object.
+;       Use: The identification of the movable object.
 ; Output: n/a
-; Use: Setting the identification of a driving object.
+; Use: Setting the identification of a movable object.
 ;-------------------------------------------------------
 
     (define/public (setID! symbol)
       (if (and (eq? ID 'uninitialised)
                (symbol? symbol))
           (set! ID symbol)
-          (error "DrivingObject% setID!: ID already initialised or contract violation, expected symbol received" symbol)))
+          (error "MoveableObject% setID!: ID already initialised or contract violation, expected symbol received" symbol)))
 
 ;------------------------------------------------
 ; Funcion: getID
 ; Parameters: n/a
 ; Output:
 ;    ID: symbol
-;     Use: The ID of the driving object.
-; Use: Retreiving the ID of the driving object.
+;     Use: The ID of the movable object.
+; Use: Retreiving the ID of the movable object.
 ;------------------------------------------------
 
     (define/public (getID)
       (if (initialised?)
           ID
-          (error "DrivingObject% getID: object not initialised, please initialise before use" ID)))
+          (error "MoveableObject% getID: object not initialised, please initialise before use" ID)))
 
 ;----------------------------------------------------------------------------------
 ; Function: setPredecessorID!
 ; Parameters:
 ;      symbol: symbol
-;        Use: The identification of the predecessor of the current driving object.
+;        Use: The identification of the predecessor of the current movable object.
 ; Output: n/a
-; Use: Setting the identification of the predecessing driving object.
+; Use: Setting the identification of the predecessing movable object.
 ;----------------------------------------------------------------------------------
 
     (define/public (setPredecessorID! symbol)
       (if (symbol? symbol)
           (set! predecessorID symbol)
-          (error "DrivingObject% setPredecessorID!: contract violation expected symbol received" symbol)))
+          (error "MoveableObject% setPredecessorID!: contract violation expected symbol received" symbol)))
 
 ;---------------------------------------------------------------------------
 ; Function: getPredecessorID
 ; Parameters: n/a
 ; Output:
 ;    predecessorID: symbol
-;        Use: The identification of the predecessing driving object.
-; Use: Retrieving the identification of the predecessing driving object.
+;        Use: The identification of the predecessing movable object.
+; Use: Retrieving the identification of the predecessing movable object.
 ;---------------------------------------------------------------------------
 
     (define/public (getPredecessorID)
       (if (initialised?)
           predecessorID
-          (error "DrivingObject% getPredecessorID: object not initialised, please initialise before use" ID)))
+          (error "MoveableObject% getPredecessorID: object not initialised, please initialise before use" ID)))
 
 ;------------------------------------------------------------------
 ; Function: setSuccessorID!
 ; Parameters:
 ;     symbol: symbol
-;       Use: The identification of the successing driving object.
+;       Use: The identification of the successing movable object.
 ; Output: n/a
-; Use: Setting the successing driving object's identification.
+; Use: Setting the successing movable object's identification.
 ;------------------------------------------------------------------
 
     (define/public (setSuccessorID! symbol)
       (if (symbol? symbol)
           (set! successorID symbol)
-          (error "DrivingObject% setSuccessorID!: contract violation expected symbol received" symbol)))
+          (error "MoveableObject% setSuccessorID!: contract violation expected symbol received" symbol)))
 
 ;---------------------------------------------------------------------
 ; Function: getSuccessorID
 ; Parameters: n/a
 ; Output:
 ;    successorID: symbol
-;      Use: The identification of the successing driving object.
-; Use: Retrieve the identification of the successing driving object.
+;      Use: The identification of the successing movable object.
+; Use: Retrieve the identification of the successing movable object.
 ;---------------------------------------------------------------------
 
     (define/public (getSuccessorID)
       (if (initialised?)
           successorID
-          (error "DrivingObject% getSuccessorID: object not initialised, please initialise before use")))
+          (error "MoveableObject% getSuccessorID: object not initialised, please initialise before use")))
 
 ;-----------------------------------------------------------------------------------------
 ; Function: setTrainID!
 ; Parameters:
 ;    symbol: symbol
-;      Use: The identification of the train where the driving object is associated with.
+;      Use: The identification of the train where the movable object is associated with.
 ; Output: n/a
-; Use: Setting the identification associating train of the driving object.
+; Use: Setting the identification associating train of the movable object.
 ;-----------------------------------------------------------------------------------------
 
     (define/public (setTrainID! symbol)
       (if (symbol? symbol)
           (set! trainID symbol)
-          (error "DrivingObject% setTrainID!: contract violation, expected symbol received" symbol)))
+          (error "MoveableObject% setTrainID!: contract violation, expected symbol received" symbol)))
 
 ;-------------------------------------------------------------
 ; Function: getTrainID
@@ -148,7 +148,7 @@
     (define/public (getTrainID)
       (if (initialised?)
           trainID
-          (error "DrivingObject% getTrainID: object is not initialised, please initialise before use" ID)))
+          (error "MoveableObject% getTrainID: object is not initialised, please initialise before use" ID)))
 
 ;------------------------------------------------------------------------------------------------
 ; Function: couple!
@@ -156,22 +156,22 @@
 ;    train: symbol
 ;      Use: The identification of the associating train. This can not be 'none
 ;    pre: symbol
-;     Use: The identification of the predecessing driving object.
+;     Use: The identification of the predecessing moveable object.
 ;    succ: symbol
-;     Use: The identificationf of the successing driving object.
-; Use: Connecting a driving object to a train, and potentionally a successor and predecessor.
+;     Use: The identificationf of the successing movable object.
+; Use: Connecting a movable object to a train, and potentionally a successor and predecessor.
 ;      if there isn't a successor and predecessor this can be set to the symbol 'none. 
 ;------------------------------------------------------------------------------------------------
 
     (define/public (couple! train pre succ)
-      (if (and (and (symbol? train)(not (eq? train 'none)))   ; A driving object must be connected to a train, this can't be none
+      (if (and (and (symbol? train)(not (eq? train 'none)))   ; A moveable object must be connected to a train, this can't be none
                (symbol? pre)
                (symbol? succ))
           (begin
             (set! trainID train)
             (set! predecessorID pre)
             (set! successorID succ))
-          (error "DrivingObject% couple!: contract violation symbol expected received" train pre succ)))
+          (error "MoveableObject% couple!: contract violation symbol expected received" train pre succ)))
 
    
     ))
