400fbf316d7ad3b32f2f94c90462aa5d0690408e
unknown <asmaoualmakran@gmail.com>
2019-03-26 14:24:00 +0100

    FileReader% function hashtable added + manipulator for adding deleting and updating functions to the table added + callFuncion completed

--- Infrabel/fileReader.rkt
+++ Infrabel/fileReader.rkt
@@ -11,7 +11,56 @@
     (super-new)
 
     
-   
+    (define functionHash (make-hash))  ;string is key value is a lambda
+
+    (define/public (add! key function)
+      (if (and (string? key)
+               (procedure? function))
+          (if (not (hash-has-key? key))
+              (hash-set! functionHash key function)
+              (error "FileReader% add!: Given key is already member of the hashtable, recieved:" key))
+          (error "FileReader% add!: Contract violation, expected a string and a function recieved:" key function)))
+
+    (define/public (update! key function)
+      (if (and (string? key)
+               (procedure? function))
+          (if (hash-has-key? functionHash key)
+               (hash-set! functionHash key function)
+              (error "FileReader% update!: Given key does not belong to a function, can not update the function, recieved:" key))
+          (error "FileReader% update!: Contract violation expected a string and a procedure, recieved" key function)))
+               
+
+    ;----------------------------------------------------------------
+    ; Function: delete!
+    ; Parameters:
+    ;       key: string
+    ;        Use: The key of the function that needs to be deleted.
+    ; Output: n/a
+    ; Use: Delete a function using it's key.
+    ;----------------------------------------------------------------
+
+    (define/public (delete! key)
+      (if (string? key)
+          (if (hash-has-key? functionHash key)
+              (hash-remove! functionHash key)
+              (error "FileReader delete!: Hashtable does not contain given key, recieved:" key))
+          (error "FileReader% delete!: Contract violation, expected a string recieved:" key)))
+
+    ;---------------------------------------------------------------------
+    ; Function: member?
+    ; Parameters:
+    ;        key: string
+    ;         Use: The key of the function that needs to be checked.
+    ; Output:
+    ;        boolean: boolean
+    ;         Use: Determine if the function is in the hashtable.
+    ; Use: Determine if the given key of a function is in the hashtable
+    ;---------------------------------------------------------------------
+    
+    (define/public (member? key)
+      (if (string? key)
+          (hash-has-key? functionHash key)
+          (error "FileReader% member?: Contract violation, expected a string recieved:" key)))
 
     ;-------------------------------------------------------------------------------
     ; Function: loadRailway
@@ -25,7 +74,7 @@
     ;-------------------------------------------------------------------------------
 
     (define/public (loadRailway file) ;manager)
-     (define in (open-input-file file))  ;no flags needed the default mode is sufficient
+      (define in (open-input-file file))  ;no flags needed the default mode is sufficient
       (readFile file)
       
       )
@@ -34,38 +83,23 @@
     (define/private (readFile file)
       (define in (open-input-file file))
       (for ([l (in-lines in)])
-            (display l)
-            (newline)))
+        (display l)
+        (newline)))
 
- ;   (define/private (parseString string)
- ;     (let ([count 0]
- ;           [i 0]
- ;           [j 0])
- ;       (for ([s string])
- ;            (while (and (> count 0) (>= i 0))  ;i >= 0 so that the loop will start 
- ;                   (cond ((eq? (vector-ref string i) "(") (begin (set! count (+ count 1))  ; if you read a ( or ) skip index, add or substract counter
- ;                                                                 (set! i (+ i 1))))
- ;                         ((eq? (vector-ref string i) ")") (begin (set! count (- count 1))  ; --> ) means also end of word, not only expression 
- ;                                                                 (set! i (+ i 1))))
- ;                         ((eq? (vector-ref string j) "(") (begin (set! count (+ count 1))
- ;                                                                 (set! j (+ j 1))))
- ;                         ((eq? (vector-ref string j) ")") (begin (set! count (- count 1))
- ;                                                                 (set! j (+ j 1))))
- ;                         
- ;                         (else (error "FileReader% parseString: invalid expression, cannot be parsed")))
- ;            
- ;       ))))
+
 
     ;-----------------------------------------------------------------------------------
     ; Function: parseString
     ; Parameters:
     ;        string: string
     ;          Use: The string that needs to be parsed.
+    ;        manager: object: RailwayManager%
+    ;          Use: The railway manager that manages the railway at that time.
     ; Output: n/a 
     ; Use: Parse a string and call the appropriate function, determined by parsing it.
     ;-----------------------------------------------------------------------------------
 
-    (define/private (parseString string)
+    (define/private (parseString string manager)
       (for ([char string])
         (let ([count 0]
               [i 0]
@@ -76,21 +110,35 @@
                      (set! i (+ i 1))
 
                      (while (> count 0)       ;loop where the indexes and counters are manipulated
-                            (if (not (eq? (vector-ref string j) " "))
+                            (if (not (eq? (vector-ref string j) " "))    ;TODO, refine the situations!!!!!!!
                                 (set! j (+ j 1))
-                                 (set! result (result append (substring string i j))))))
-              (error "FileReader% parseString: No opening ( in expression, it is not a function call expected function call.")))))
+                                (set! result (result append (substring string i j))))))
+              (error "FileReader% parseString: No opening ( in expression, it is not a function call expected function call."))
+          (if (not (null? result))
+              (callFunction result)
+              (error "FileReader% parseString: Variable result is an empty list, no function to be called")))))
 
-    ;--------------------------------------------------------------
+    ;----------------------------------------------------------------------------------------
     ; Function: callFunction
     ; Parameters:
     ;        lst: list<string>
+    ;         Use: The list that contains the parsed string which contains a function call.
     ; Output: n/a
     ; Use: Call the correct functions using the given string list
-    ;--------------------------------------------------------------
+    ;----------------------------------------------------------------------------------------
               
-    (define/private (callFunction lst)
-      'test)
-      ))
+    (define/private (callFunction lst manager)
+      (if (and (not (null? lst))
+               (list? lst))
+          (let ([function 0]
+                [pms '()])   ;Keep the parameters in a list
+            (for ([elm lst])
+              (cond ((eq? (car lst)) 'do)
+                    (else (error "FileReader% callFunction: procedure is unknown cannot execute"(car lst))))))
+          (error "FileReader% callFunction: Contract violation expected non-empty list recieved"lst)))
 
     
+    
+    
+    ))
+
