fc79042d9357e4f0ed4303cb2bd155e3114438be
unknown <asmaoualmakran@gmail.com>
2019-03-28 14:24:57 +0100

    methods added + comments added, still bugs accessing files using path system

--- Infrabel/fileReader.rkt
+++ Infrabel/fileReader.rkt
@@ -4,6 +4,10 @@
 
 (require "railwayManager.rkt")
 
+
+(define manager (new RailwayManager%))
+
+
 (provide FileReader%)
 
 (define FileReader%
@@ -11,9 +15,55 @@
     (super-new)
 
     ; Hashstable used to save functions
+    (define functionHash (make-hash))
     
-    (define functionHash (make-hash))  
+    (define managerType 'object:RailwayManager%)
 
+    (field [railmanager 'none])
+
+    ;-----------------------------------------------------------
+    ; Function: initialise!
+    ; Parameters:
+    ;          manager: object:RailwayManager%
+    ;            Use: The railway manager that needs to be set
+    ; Output: n/a
+    ; Use: Initialise the object with a railway manager
+    ;------------------------------------------------------------
+    
+    (define/public (initialise! manager)
+      (if (not (initialised?))
+          (if (eq? managerType (object-name manager))
+              (begin (set! railmanager manager)
+                     (addBasicFunctions!))
+              (error "FileReader% initialise!: Contract violation expected object:RailwayManager% recieved:" manager))
+          (error "FileReader% initialise!: object is already initialised")))
+
+    ;-------------------------------------------------------
+    ; Function: initialised?
+    ; Parameters: n/a
+    ; Output:
+    ;        boolean: boolean
+    ;         Use: Determine if the object is initialised
+    ; Use: Determine if the object is initialised
+    ;-------------------------------------------------------
+    
+    (define/public (initialised?)
+      (not (eq? railmanager 'none)))
+
+    ;-------------------------------------------------
+    ; Function: getManager
+    ; Parameters: n/a
+    ; Output:
+    ;       manager: object:RailwayManager%
+    ;         Use: The railway manager that is used
+    ; Use: Retrieve the used railway manager
+    ;-------------------------------------------------
+
+    (define/public (getManager)
+      (if (initialised?)
+          railmanager
+          (error "FileReader% getManager: Object is not initialised please initialise before use")))
+    
     ;-----------------------------------------------------------
     ; Function: add!
     ; Parameters:
@@ -28,7 +78,7 @@
     (define/public (add! key function)
       (if (and (string? key)
                (procedure? function))
-          (if (not (hash-has-key? key))
+          (if (not (hash-has-key? functionHash key))
               (hash-set! functionHash key function)
               (error "FileReader% add!: Given key is already member of the hashtable, recieved:" key))
           (error "FileReader% add!: Contract violation, expected a string and a function recieved:" key function)))
@@ -47,7 +97,7 @@
     (define/public (update! key function)
       (if (and (string? key)
                (procedure? function))
-          (if (hash-has-key? functionHash key)
+          (if (member? key)
                (hash-set! functionHash key function)
               (error "FileReader% update!: Given key does not belong to a function, can not update the function, recieved:" key))
           (error "FileReader% update!: Contract violation expected a string and a procedure, recieved" key function)))
@@ -85,6 +135,26 @@
           (hash-has-key? functionHash key)
           (error "FileReader% member?: Contract violation, expected a string recieved:" key)))
 
+
+    ;-------------------------------------------------------------------
+    ; Function: getFunc
+    ; Parameters:
+    ;         key: string
+    ;          Use: The key used to hash the function in the hashtable
+    ; Output:
+    ;         func: #<procedure>
+    ;          Use: The function that needs to be retrieved
+    ; Use: Retrieve a function from the hashtable using it's key.
+    ;-------------------------------------------------------------------
+    
+    (define/public (getFunc key)
+      (if (string? key)
+          (if (member? key)
+              (hash-ref functionHash key)
+              (error "FileReader%: Hashtable has no such key, recieved:" key))
+          (error "FileReader% getKey: Contract violation, expected a string recieved:" key)))
+          
+
     ;-------------------------------------------------------------------------------
     ; Function: loadRailway
     ; Parameters:
@@ -96,20 +166,23 @@
     ; Use: Read a file where the railway is saved and create the correct objects.
     ;-------------------------------------------------------------------------------
 
-    (define/public (loadRailway file) ;manager)
-      (define in (open-input-file file))  ;no flags needed the default mode is sufficient
-      (readFile file)
-      
-      )
+    (define file  "C:\\Users\\Asma\\GitHub\\PP2\\railwaySetup\\hardware.rkt")
+    (define/public (loadRailway ) ;file)
+      (readFile file))
 
-
+    ;---------------------------------------------------------------
+    ; Function: readFile
+    ; Parameters:
+    ;         file: string
+    ;          Use: The path location of the to be read file
+    ; Output: n/a
+    ; Use: Read the file, parse it and call the correct functions
+    ;---------------------------------------------------------------
+    
     (define/private (readFile file)
       (define in (open-input-file file))
       (for ([l (in-lines in)])
-        (display l)
-        (newline)))
-
-
+        (callFunction(parseString l))))
 
     ;-----------------------------------------------------------------------------------
     ; Function: parseString
@@ -122,7 +195,7 @@
     ; Use: Parse a string and call the appropriate function, determined by parsing it.
     ;-----------------------------------------------------------------------------------
 
-    (define/private (parseString string manager)
+    (define/private (parseString string)
       (for ([char string])
         (let ([count 0]
               [i 0]
@@ -150,16 +223,36 @@
     ; Use: Call the correct functions using the given string list
     ;----------------------------------------------------------------------------------------
               
-    (define/private (callFunction lst manager)
-      (if (and (not (null? lst))
-               (list? lst))
-          (let ([function 0]
-                [pms '()])   ;Keep the parameters in a list
-            (for ([elm lst])
-              (cond ((eq? (car lst)) 'do)
-                    (else (error "FileReader% callFunction: procedure is unknown cannot execute"(car lst))))))
-          (error "FileReader% callFunction: Contract violation expected non-empty list recieved"lst)))
+    (define/private (callFunction lst)
+      (if (member? (car lst))
+          (let ([func (getFunc (car lst))]
+                [name (cadr lst)]
+                [connections cddr])
+            (getFunc name railmanager . connections))
+          (error "FileReader% callFunction: Given hashtable does not contain the given key, recieved:" (car lst)) ))
 
+    ;----------------------------------------------------------------------
+    ; Function: addBasicFunctions!
+    ; Parameters: n/a
+    ; Output: n/a
+    ; Use: Add functions for ceating detection block, switches and tracks
+    ;----------------------------------------------------------------------
+
+    (define/public (addBasicFunctions!)
+      (if (initialised?)
+      (begin(add! "block%" (lambda (name railmanager)
+                      (send railmanager createBlock! name)))
+      
+      (add! "switch%" (lambda (name railmanager connections)
+                        (send railmanager createSwitch! name)
+                        (send railmanager initSwitch! name (cons 0 0) 'left 'left 10 (length connections))
+                        (send (send railmanager getSwitch name) initconnections! connections)))
+
+      (add! "track%" (lambda (name railmanager connections)
+                       (send railmanager createTrack! name connections)
+                       (send railmanager initTrack! name (cons 0 0) 0 0 0 ))))
+      
+      (error "FileReader% addBasicFunctions!: Object is not initialised, please initialise before use")))
     
     
     
