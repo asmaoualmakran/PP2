bc413271eecd1272cc7c30c9abfe67ff3641a8da
unknown <asmaoualmakran@gmail.com>
2019-05-07 12:35:11 +0200

    Deletion and failure tests completed integration-tests

--- Tests/integration-tests.rkt
+++ Tests/integration-tests.rkt
@@ -40,18 +40,42 @@
           [getTrack (send railwayManager getTrack 't)]
           [getBlock (send railwayManager getDetectionblock 'd)])
 
+      (test-case
+       "Creation and initialisation tests"
+
+       (check-true (send railwayManager initialised?))
+       (check-eq? (send railwayManager getObject 's) getSwitch)
+       (check-eq? (send railwayManager getObject 't) getTrack)
+       (check-eq? (send railwayManager getObject 'd) getBlock)
+       (check-true (send getSwitch initialised?))
+       (check-true (send getTrack  initialised?))
+       (check-true (send getBlock  initialised?))
+       (check-true (send railwayManager isSwitch? 's))
+       (check-true (send railwayManager isTrack? 't))
+       (check-true (send railwayManager isDetectionblock? 'd))
+       )))
+
+  (send railwayManager deleteSwitch! 's)
+  (send railwayManager deleteTrack! 't)
+  (send railwayManager deleteDetectionblock! 'd)
+
   (test-case
-   "Creation and initialisation tests"
+   "Deletion tests"
 
-   (check-true (send railwayManager initialised?))
-   (check-eq? (send railwayManager getObject 's) getSwitch)
-   (check-eq? (send railwayManager getObject 't) getTrack)
-   (check-eq? (send railwayManager getObject 'd) getBlock)
-   (check-true (send getSwitch initialised?))
-   (check-true (send getTrack  initialised?))
-   (check-true (send getBlock  initialised?))
-   ))))
+  (check-false (send railwayManager isSwitch? 's))
+  (check-false (send railwayManager isTrack? 't))
+  (check-false (send railwayManager isDetectionblock? 'd)))
 
+  (test-case
+   "Dubble deletion tests"
+   
+  (check-exn exn:fail?
+             (lambda () (send railwayManager deleteSwitch! 's)))
+  (check-exn exn:fail?
+             (lambda () (send railwayManager deleteTrack! 't)))
+  (check-exn exn:fail?
+             (lambda () (send railwayManager deleteDetectionblock! 'd)))          
+   ))
 
 ;------------------------------------------
 ; Failure tests
@@ -61,26 +85,48 @@
 (let ([railwayManager (new RailwayManager%)]
       [trainManager (new TrainManager%)]
       [graph (unweighted-graph/directed '((a b) (c d)))])
-
-  (test-case
-   "Failure test railway manager wrong init pm"
-
-   (check-false (send railwayManager initialised?))
-   (check-exn exn:fail?
-              (lambda () (send railwayManager getGraph)))
-   (check-exn exn:fail?
-              (lambda () (send railwayManager initialise! 5 4))))
-
-  (send railwayManager initialise! graph trainManager)
   
-  (test-case
-   "Failure test railway manager wrong creation pm"
+  (let([switch (send railwayManager createSwitch! 's)]
+       [track (send railwayManager createTrack! 't)]
+       [detectionblock (send railwayManager createDetectionblock! 'd)])
+
+    (test-case
+     "Failure test railway manager wrong init pm"
+
+     (check-false (send railwayManager initialised?))
+     (check-exn exn:fail?
+                (lambda () (send railwayManager getGraph)))
+     (check-exn exn:fail?
+                (lambda () (send railwayManager initialise! 5 4))))
+
+    (send railwayManager initialise! graph trainManager)
+  
+    (test-case
+     "Failure test railway manager wrong creation pm"
    
-   (check-exn exn:fail?
-              (lambda () (send railwayManager createDetectionblock! 5)))
-   (check-exn exn:fail?
-              (lambda () (send railwayManager createSwitch! 5)))
-   (check-exn exn:fail?
-              (lambda () (send railwayManager createTrack! 5)))))
-   
+     (check-exn exn:fail?
+                (lambda () (send railwayManager createDetectionblock! 5)))
+     (check-exn exn:fail?
+                (lambda () (send railwayManager createSwitch! 5)))
+     (check-exn exn:fail?
+                (lambda () (send railwayManager createTrack! 5))))
+
+    (test-case
+     "Failure test wrong deletion pm"
+
+     (check-exn exn:fail?
+                (lambda () (send railwayManager deleteDetecionblock! 4)))
+     (check-exn exn:fail?
+                (lambda () (send railwayManager deleteDetectionblock! 't)))
+
+     (check-exn exn:fail?
+                (lambda () (send railwayManager deleteTrack! 5)))
+     (check-exn exn:fail?
+                (lambda () (send railwayManager deleteTrack! 'd)))
+
+     (check-exn exn:fail?
+                (lambda () (send railwayManager deleteSwitch! 5)))
+     (check-exn exn:fail?
+                (lambda () (send railwayManager deleteSwitch! 'd)))
+     )))
    
